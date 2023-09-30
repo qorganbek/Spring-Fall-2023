45b1628949cae37edf421a5d5930ae4a3261727c
qorganbek <dimashqorganbek@gmail.com>
2023-09-30 11:49:49 +0600

    Async

--- sis3/spring-events/src/main/java/com/example/springevents/service/PrintedEventHandler.java
+++ sis3/spring-events/src/main/java/com/example/springevents/service/PrintedEventHandler.java
@@ -4,6 +4,7 @@ import com.example.springevents.domain.event.PrintedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class PrintedEventHandler {
 
     @EventListener
+    @Async
     public void process(PrintedEvent event) {
         PrintedEvent printedEvent = new PrintedEvent();
         printedEvent.setMessage(printedEvent.getMessage());
